922514d6147df8619ada43067271039ff811b847
iprocastinate <pradyumnasarkar4@gmail.com>
2026-03-01 19:12:31 +0530

    Update tordownload.py

--- bot/core/tordownload.py
+++ bot/core/tordownload.py
@@ -1,43 +1,74 @@
+from time import time, sleep
+from traceback import format_exc
+from math import floor
 from os import path as ospath
-from aiofiles import open as aiopen
-from aiofiles.os import path as aiopath, remove as aioremove, mkdir
+from aiofiles.os import remove as aioremove
+from pyrogram.errors import FloodWait
 
-from aiohttp import ClientSession
-from torrentp import TorrentDownloader
-from bot import LOGS
-from bot.core.func_utils import handle_logs
+from bot import bot, Var
+from .func_utils import editMessage, sendMessage, convertBytes, convertTime
+from .reporter import rep
 
-class TorDownloader:
-    def __init__(self, path="."):
-        self.__downdir = path
-        self.__torpath = "torrents/"
+class TgUploader:
+    def __init__(self, message):
+        self.cancelled = False
+        self.message = message
+        self.__name = ""
+        self.__qual = ""
+        self.__client = bot
+        self.__start = time()
+        self.__updater = time()
+
+    async def upload(self, path, qual):
+        self.__name = ospath.basename(path)
+        self.__qual = qual
+        try:
+            if Var.AS_DOC:
+                return await self.__client.send_document(chat_id=Var.FILE_STORE,
+                    document=path,
+                    thumb="thumb.jpg" if ospath.exists("thumb.jpg") else None,
+                    caption=f"<b>{self.__name}</b>",
+                    force_document=True,
+                    progress=self.progress_status
+                )
+            else:
+                return await self.__client.send_video(chat_id=Var.FILE_STORE,
+                    video=path,
+                    thumb="thumb.jpg" if ospath.exists("thumb.jpg") else None,
+                    caption=f"<b>{self.__name}</b>",
+                    progress=self.progress_status
+                )
+        except FloodWait as e:
+            sleep(e.value * 1.5)
+            return await self.upload(path, qual)
+        except Exception as e:
+            await rep.report(format_exc(), "error")
+            raise e
+        finally:
+            await aioremove(path)
+
+    async def progress_status(self, current, total):
+        if self.cancelled:
+            self.__client.stop_transmission()
+        now = time()
+        diff = now - self.__start
+        if (now - self.__updater) >= 7 or current == total:
+            self.__updater = now
+            percent = round(current / total * 100, 2)
+            speed = current / diff 
+            eta = round((total - current) / speed)
+            bar = floor(percent/8)*"█" + (12 - floor(percent/8))*"▒"
+            progress_str = f"""‣ <b>Anime Name :</b> <b><i>{self.__name}</i></b>
+
+‣ <b>Status :</b> <i>Uploading</i>
+    <code>[{bar}]</code> {percent}%
     
-    @handle_logs
-    async def download(self, torrent, name=None):
-        if torrent.startswith("magnet:"):
-            torp = TorrentDownloader(torrent, self.__downdir)
-            await torp.start_download()
-            return ospath.join(self.__downdir, name)
-        elif torfile := await self.get_torfile(torrent):
-            torp = TorrentDownloader(torfile, self.__downdir)
-            await torp.start_download()
-            await aioremove(torfile)
-            return ospath.join(self.__downdir, torp._torrent_info._info.name())
+    ‣ <b>Size :</b> {convertBytes(current)} out of ~ {convertBytes(total)}
+    ‣ <b>Speed :</b> {convertBytes(speed)}/s
+    ‣ <b>Time Took :</b> {convertTime(diff)}
+    ‣ <b>Time Left :</b> {convertTime(eta)}
 
-    @handle_logs
-    async def get_torfile(self, url):
-        if not await aiopath.isdir(self.__torpath):
-            await mkdir(self.__torpath)
-        
-        tor_name = url.split('/')[-1]
-        des_dir = ospath.join(self.__torpath, tor_name)
-        
-        async with ClientSession() as session:
-            async with session.get(url) as response:
-                if response.status == 200:
-                    async with aiopen(des_dir, 'wb') as file:
-                        async for chunk in response.content.iter_any():
-                            await file.write(chunk)
-                    return des_dir
-        return None
-        
+‣ <b>File(s) Encoded:</b> <code>{Var.QUALS.index(self.__qual)} / {len(Var.QUALS)}</code>"""
+            
+            if self.message:
+                await editMessage(self.message, progress_str)
